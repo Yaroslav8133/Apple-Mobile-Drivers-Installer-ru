09e214c5c6218d9e24d44b0842651e84348a3c6e
Yaroslav8133 <142659569+Yaroslav8133@users.noreply.github.com>
2025-10-22 23:32:22 +0700

    Update AppleDrivInstaller.ps1
    Link update

--- AppleDrivInstaller.ps1
+++ AppleDrivInstaller.ps1
@@ -4,8 +4,8 @@
 ## Download links for Apple USB Drivers and Apple Mobile Ethernet USB Drivers respectively.
 ## All of these are downloaded from Microsoft's Update Catalog, which you can browse yourself at here: https://www.catalog.update.microsoft.com/
 
-$AppleDri1 = "https://catalog.s.download.windowsupdate.com/d/msdownload/update/driver/drvs/2020/11/01d96dfd-2f6f-46f7-8bc3-fd82088996d2_a31ff7000e504855b3fa124bf27b3fe5bc4d0893.cab"
-$AppleDri2 = "https://catalog.s.download.windowsupdate.com/c/msdownload/update/driver/drvs/2017/11/netaapl_7503681835e08ce761c52858949731761e1fa5a1.cab"
+$AppleDri1 = "https://downloader.disk.yandex.ru/disk/e92f44ff206c577c656fa54d731aa568fb5d4ee9624e04e977388088616b344c/68f93eb0/Q2aE2Ho4XamG6jq-JEKFwtlI8OPM_sB7r_21eXNHWrrNjdvuAeDJ409W9Fda1i18nSoMw_ssyJSfDsGYn0rnMg%3D%3D?uid=0&filename=01d96dfd-2f6f-46f7-8bc3-fd82088996d2_a31ff7000e504855b3fa124bf27b3fe5bc4d0893.cab&disposition=attachment&hash=7b22J6/1yPtrrXkVsMsWSXtXclX3o0UHlNvw8cKJ000PaBGF5mvHkIetgB3gDb8Aq/J6bpmRyOJonT3VoXnDag%3D%3D&limit=0&content_type=application%2Fvnd.ms-cab-compressed&owner_uid=1020185368&fsize=88728&hid=6fe41749b87bbe1dcf6dd5854baa237b&media_type=unknown&tknv=v3"
+$AppleDri2 = "https://downloader.disk.yandex.ru/disk/f1ec22cce699f3d7808897e6dce038042176d063adf2f3a555109d8e9f61a87a/68f93e7e/Q2aE2Ho4XamG6jq-JEKFwrChSx2v-en8K3X_MuUP3BBh5KQgy8JV-qpFG7GpE-jhRz3oWRloIk6LKgUI1WEAiw%3D%3D?uid=0&filename=netaapl_7503681835e08ce761c52858949731761e1fa5a1.cab&disposition=attachment&hash=aMYICgv24xWgu21tpoFltmAXBFj0WQ/FLuY%2BU6eRcbAZUeh5rmXl9TdMQS11FwOcq/J6bpmRyOJonT3VoXnDag%3D%3D&limit=0&content_type=application%2Fvnd.ms-cab-compressed&owner_uid=1020185368&fsize=1833386&hid=d75a8cabc564f085df0c61ab8dc85662&media_type=unknown&tknv=v3"
 $AppleITunesLink = "https://www.apple.com/itunes/download/win64"
 
 Write-Host ""
@@ -64,4 +64,5 @@ try {
 
 Write-Host ""
 Write-Host -ForegroundColor Cyan "Installation complete! Enjoy your Apple devices!!"
+
 Write-Host -ForegroundColor Yellow "(If devices are still not working, a reboot might be needed!!)"
